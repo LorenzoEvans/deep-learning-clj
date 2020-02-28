789941679d33f6a515b728cada513d66ee4a50ee
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-28 16:25:46 -0500

    alright lets go, reading csvs

--- src/clj_boost_demo/core.clj
+++ src/clj_boost_demo/core.clj
@@ -1,6 +1,12 @@
-(ns clj-boost-demo.core)
+(ns clj-boost-demo.core
+  (:require [clj-boost.core :as boost]
+            [clojure.java.io :as io]
+            [clojure.data.csv :as csv]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def iris-data "resources/resources_iris.csv")
+
+(defn generate-iris [iris-data]
+  (with-open [reader (io/reder iris-data)]
+    (into []
+      (comp (drop 1) (map #(split-at 4 %)))
+      (csv/read-csv reader))))
